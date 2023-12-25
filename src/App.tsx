ba6adbee8371d36bb9027d930870bbc9c7052472
Cansin Yildiz <cansinyildiz@gmail.com>
2023-12-25 17:32:05 +0300

    Introduce TypeScript support.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Button } from 'flowbite-react';
 
 export default function App() {
     return (
-        <div>
-            <Button>Click me</Button>
-        </div>
+        <Button>Click me</Button>
     );
 }
