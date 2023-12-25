9586d4ef071e66ac2ff2d8bf00c8914de1a754e8
Cansin Yildiz <cansinyildiz@gmail.com>
2023-12-26 02:00:02 +0300

    Better 404 page visuals.

--- src/FourOhFour/FourOhFour.tsx
+++ src/FourOhFour/FourOhFour.tsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function FourOhFour() {
-  return <div className="grow p-4">404 content will go here...</div>;
+  return (
+    <main className="px-12 py-8 mx-auto max-w-7xl">
+      <div className="format dark:format-invert max-w-full pb-8">
+        <h1>404 - Page Not Found</h1>
+        <p>
+          Oops! The page you are looking for doesn't exist or has been moved.
+        </p>
+        <p>
+          <Link to="/">Go back to Home</Link>
+        </p>
+      </div>
+    </main>
+  );
 }
