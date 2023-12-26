a212b161866bccd5c6964f14bab9b1e90af28f23
Cansin Yildiz <cansinyildiz@gmail.com>
2023-12-26 18:03:42 +0300

    Simplify how you render PDF files.

--- src/Vault/File/Page.tsx
+++ src/Vault/File/Page.tsx
@@ -1,16 +1,11 @@
-import { Document, pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url,
-).toString();
-
 export default function FilePage() {
   return (
     <div className="p-4 grow overflow-y-auto">
-      <Document
-        file="https://www.learningcontainer.com/wp-content/uploads/2019/09/sample-pdf-file.pdf"
-        options={{ withCredentials: true }}
+      <embed
+        src="https://cdn.filestackcontent.com/O7oBXd8hRfW9cyVpqbe6"
+        width="100%"
+        height="100%"
+        type="application/pdf"
       />
     </div>
   );
