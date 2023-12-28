0e347ea6119145f6d38ee6a6c8b7cb4bde777ab1
Cansin Yildiz <cansinyildiz@gmail.com>
2023-12-28 03:32:11 +0300

    Fix all TypeScript errors.

--- src/Vault/common/Layout/Navigation/Collapse.tsx
+++ src/Vault/common/Layout/Navigation/Collapse.tsx
@@ -1,12 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import type { SidebarCollapseProps as FlowbiteSidebarCollapseProps } from "flowbite-react";
-import type { ReactNode } from "react";
 
-interface SidebarCollapseProps
-  extends Omit<FlowbiteSidebarCollapseProps, "label"> {
-  label?: ReactNode;
-}
-
-export default function SidebarCollapse(props: SidebarCollapseProps) {
+export default function SidebarCollapse(props) {
   return <Sidebar.Collapse {...props} />;
 }
